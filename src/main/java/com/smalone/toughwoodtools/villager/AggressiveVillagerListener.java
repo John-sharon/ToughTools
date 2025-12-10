eed93175ef9a3abbdf4c91aa04a8317f76ab8a65
John-sharon <s202375@pcsdny.org>
2025-12-10 08:59:37 -0500

    Create AggressiveVillagerListener.java

---
+++ src/main/java/com/smalone/toughwoodtools/villager/AggressiveVillagerListener.java
@@ -0,0 +1,67 @@
+// src/main/java/com/smalone/toughwoodtools/villager/AggressiveVillagerListener.java
+package com.smalone.toughwoodtools.villager;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import com.smalone.toughwoodtools.ToughTools;
+
+/**
+ * Listener that triggers villager aggression according to plugin config.
+ */
+public class AggressiveVillagerListener implements Listener {
+
+    private final ToughTools plugin;
+    private final AggroManager manager;
+
+    public AggressiveVillagerListener(ToughTools plugin, AggroManager manager) {
+        this.plugin = plugin;
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!plugin.getConfig().getBoolean("villager-aggro.enabled", true)) return;
+
+        Entity entity = event.getEntity();
+        if (!(entity instanceof Villager)) return;
+
+        if (!(event.getDamager() instanceof Player)) return;
+        Player damager = (Player) event.getDamager();
+
+        String trigger = plugin.getConfig().getString("villager-aggro.trigger", "retaliate");
+        if ("retaliate".equalsIgnoreCase(trigger)) {
+            manager.makeAggressive((Villager) entity, damager);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        if (!plugin.getConfig().getBoolean("villager-aggro.enabled", true)) return;
+
+        Entity clicked = event.getRightClicked();
+        if (!(clicked instanceof Villager)) return;
+
+        String trigger = plugin.getConfig().getString("villager-aggro.trigger", "retaliate");
+        if ("toggle-on-interact".equalsIgnoreCase(trigger)) {
+            Villager v = (Villager) clicked;
+            Player p = event.getPlayer();
+            if (manager.isAggressive(v)) {
+                manager.removeAggro(v);
+                if (plugin.getConfig().getBoolean("villager-aggro.debug", false)) {
+                    plugin.getLogger().info("[AggressiveVillagerListener] " + p.getName() + " toggled OFF aggression for villager " + v.getUniqueId());
+                }
+            } else {
+                manager.makeAggressive(v, p);
+                if (plugin.getConfig().getBoolean("villager-aggro.debug", false)) {
+                    plugin.getLogger().info("[AggressiveVillagerListener] " + p.getName() + " toggled ON aggression for villager " + v.getUniqueId());
+                }
+            }
+        }
+    }
+}
